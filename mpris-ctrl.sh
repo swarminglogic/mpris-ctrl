74c72063c3c17d68a231f330e8c24dacec1d1d38
KeyofBlueS <45725677+KeyofBlueS@users.noreply.github.com>
2021-01-02 18:24:59 +0100

    - Revert code style/formatting

--- mpris-ctrl.sh
+++ mpris-ctrl.sh
@@ -7,8 +7,7 @@ mp_object="/org/mpris/MediaPlayer2"
 executable="$0"
 
 
-function show-help()	{
-
+function show-help {
 echo "$0 - Control MPRIS Players
 
 USAGE:
@@ -21,31 +20,27 @@ OPTIONS:
 -l, --list              Lists available NAME targets
 
 COMMANDS (ROOT):
-raise					Raises player window
-quit					Quits player
+raise                   Raises player window
+quit                    Quits player
 
 COMMANDS (PLAYER):
-play					Issues play command
-pause					Issues pause command
-stop					Issues stop command
-play-pause				Toggles between playing and pausing
-next					Plays next item in playlist
-prev[ious]				Plays previous item in playlist
-status					Displays the status of the player
-volumeup [VALUE]		Increase volume level by [VALUE].
-volumedown [VALUE]		Decrease volume level by [VALUE].
-						VALUE should be in range [0.1, 1.0].
-						If no [VALUE] is set then the default is 0.05
-volume					Get volume level.
-volume [LEVEL			Set volume level.
-						LEVEL should be in range [0.0, 1.0]
+play                    Issues play command
+pause                   Issues pause command
+stop                    Issues stop command
+play-pause              Toggles between playing and pausing
+next                    Plays next item in playlist
+prev[ious]              Plays previous item in playlist
+status                  Displays the status of the player
+volume                  Get volume level.
+volume [LEVEL]          Set volume level.
+                        LEVEL should be in range [0.0, 1.0]
 
 COMMANDS (PLAYLIST):
-tracks					Displays tracks
-add-track PATH [PLAY]	Appends file PATH to playlist.
-						PLAY should be one of {true, false(default)}
-						If true: append and skip to this item in the playlist.
-						If false, append the item to the playlist.
+tracks                  Displays tracks
+add-track PATH [PLAY]   Appends file PATH to playlist.
+                        PLAY should be one of {true, false(default)}
+                        If true: append and skip to this item in the playlist.
+                        If false, append the item to the playlist.
 
 
 PLAYER:
@@ -83,8 +78,7 @@ License:    CC-zero (public domain)
 }
 
 
-function list-valid-player-targets()	{
-
+function list-valid-player-targets {
     echo "$(dbus-send --session --dest=org.freedesktop.DBus \
         --type=method_call --print-reply /org/freedesktop/DBus \
         org.freedesktop.DBus.ListNames | grep org.mpris.MediaPlayer2 |
@@ -92,218 +86,200 @@ function list-valid-player-targets()	{
 }
 
 # $1 the player name to verify.
-function is-valid-player()	{
-
-	if list-valid-player-targets | grep -qP '^'$1'$'; then
-		return 0
-	else
-		return 1
-	fi
+function is-valid-player {
+    if list-valid-player-targets | grep -qP '^'$1'$' ; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 
-function parse-parameters()	{
-
-	while test $# -gt 0; do
-		case "$1" in
-			-h|--help)
-				showHelp 0 ;;
-			-v|--version)
-				echo "$program_name $version" ; exit 0 ;;
-			-l|--list)
-				list-valid-player-targets ; exit 0 ;;
-			*)
-				player=$1 ; shift
-				command=$1 ; shift ;
-				leftovers="$@" ; break ;;
-		esac
-	done
+function parse-parameters {
+    while test $# -gt 0; do
+        case "$1" in
+            -h|--help)
+                showHelp 0 ;;
+            -v|--version)
+                echo "$program_name $version" ; exit 0 ;;
+            -l|--list)
+                list-valid-player-targets ; exit 0 ;;
+            *)
+                player=$1 ; shift
+                command=$1 ; shift ;
+                leftovers="$@" ; break ;;
+        esac
+    done
 }
 
 # $1 interface_path
 # $2 function_name
 # $3..# parameters
-function dbus-any-command()	{
-
-	local interface_path=$1
-	local function_name=$2
-	shift 2
-	local parameters="$@"
-	dbus-send --print-reply --session \
-		--dest=org.mpris.MediaPlayer2.${player} \
-		$mp_object ${interface_path}.${function_name} ${parameters}
+function dbus-any-command {
+    local interface_path=$1
+    local function_name=$2
+    shift 2
+    local parameters="$@"
+    dbus-send --print-reply --session \
+        --dest=org.mpris.MediaPlayer2.${player} \
+        $mp_object ${interface_path}.${function_name} ${parameters}
 }
 
 # For dbus-{root,player,tracklist}-command
 # $1:function_name
 # $2..#:parameters
 function dbus-root-command {
-
-	dbus-any-command "org.mpris.MediaPlayer2" "${1}" "${@:2}"
+    dbus-any-command "org.mpris.MediaPlayer2" "${1}" "${@:2}"
 }
-
 function dbus-player-command {
-
-	dbus-any-command "org.mpris.MediaPlayer2.Player" "${1}" "${@:2}"
+    dbus-any-command "org.mpris.MediaPlayer2.Player" "${1}" "${@:2}"
 }
-
 function dbus-tracklist-command {
-
-	dbus-any-command "org.mpris.MediaPlayer2.TrackList" "${1}" "${@:2}"
+    dbus-any-command "org.mpris.MediaPlayer2.TrackList" "${1}" "${@:2}"
 }
+
 
 # $1:interface_path
 # $2:property_name
-function dbus-get-any-property()	{
-
-	dbus-send --print-reply --session \
-		--dest=org.mpris.MediaPlayer2.${player} \
-		$mp_object org.freedesktop.DBus.Properties.Get \
-		string:"$1" string:"$2"
+function dbus-get-any-property {
+    dbus-send --print-reply --session \
+        --dest=org.mpris.MediaPlayer2.${player} \
+        $mp_object org.freedesktop.DBus.Properties.Get \
+        string:"$1" string:"$2"
 }
 
 # For dbus-get-{root,player,tracklist}-property
 # $1:property_name
-function dbus-get-root-property()	{
-
-	dbus-get-any-property "org.mpris.MediaPlayer2" "${1}"
+function dbus-get-root-property {
+    dbus-get-any-property "org.mpris.MediaPlayer2" "${1}"
+}
+function dbus-get-player-property {
+    dbus-get-any-property "org.mpris.MediaPlayer2.Player" "${1}"
+}
+function dbus-get-tracklist-property {
+    dbus-get-any-property "org.mpris.MediaPlayer2.TrackList" "${1}"
 }
 
-function dbus-get-player-property()	{
-
-	dbus-get-any-property "org.mpris.MediaPlayer2.Player" "${1}"
-}
-
-function dbus-get-tracklist-property()	{
-
-	dbus-get-any-property "org.mpris.MediaPlayer2.TrackList" "${1}"
-}
 
 # $1:interface_path
 # $2:property_name
 # $3..#:parameters
-function dbus-set-any-property()	{
-
-	local interface_path=$1
-	local property_name=$2
-	shift 2
-	local parameters="$@"
-	dbus-send --print-reply --session \
-		--dest=org.mpris.MediaPlayer2.${player} \
-		$mp_object org.freedesktop.DBus.Properties.Set \
-		string:"$interface_path" string:"$property_name" "$parameters"
+function dbus-set-any-property {
+    local interface_path=$1
+    local property_name=$2
+    shift 2
+    local parameters="$@"
+    dbus-send --print-reply --session \
+        --dest=org.mpris.MediaPlayer2.${player} \
+        $mp_object org.freedesktop.DBus.Properties.Set \
+        string:"$interface_path" string:"$property_name" "$parameters"
 }
 
 # For dbus-set-{root,player,tracklist}-property
 # $1:property_name
 # $2..#:parameters
-function dbus-set-root-property()	{
-
-	dbus-set-any-property "org.mpris.MediaPlayer2" "${1}" "${@:2}"
+function dbus-set-root-property {
+    dbus-set-any-property "org.mpris.MediaPlayer2" "${1}" "${@:2}"
+}
+function dbus-set-player-property {
+    dbus-set-any-property "org.mpris.MediaPlayer2.Player" "${1}" "${@:2}"
+}
+function dbus-set-tracklist-property {
+    dbus-set-any-property "org.mpris.MediaPlayer2.TrackList" "${1}" "${@:2}"
 }
 
-function dbus-set-player-property()	{
 
-	dbus-set-any-property "org.mpris.MediaPlayer2.Player" "${1}" "${@:2}"
-}
-
-function dbus-set-tracklist-property()	{
-
-	dbus-set-any-property "org.mpris.MediaPlayer2.TrackList" "${1}" "${@:2}"
-}
-
-function execute-command()	{
-
-	case $1 in
-		raise)
-			dbus-root-command Raise			> /dev/null ;;
-		quit)
-			dbus-root-command Quit			> /dev/null ;;
-		play)
-			dbus-player-command Play		> /dev/null ;;
-		pause)
-			dbus-player-command Pause		> /dev/null ;;
-		stop)
-			dbus-player-command Stop		> /dev/null ;;
-		play-pause)
-			dbus-player-command PlayPause	> /dev/null ;;
-		next)
-			dbus-player-command Next		> /dev/null ;;
-		prev|previous)
-			dbus-player-command Previous	> /dev/null ;;
-		volumeup)
-			set -- $@
-			if [[ -z "${2}" ]];then
-				volumestep="0.05"
-			else
-				volumestep="${2}"
-			fi
-			volume_current="$(dbus-get-player-property Volume | grep double | awk '{print $3}')"
-			vulumeset="$(perl -e "print "${volume_current}" + "${volumestep}"")"
-			dbus-set-player-property Volume variant:double:"${vulumeset}" >/dev/null
-			;;
-		volumedown)
-			set -- $@
-			if [[ -z "${2}" ]];then
-				volumestep="0.05"
-			else
-				volumestep="${2}"
-			fi
-			volume_current="$(dbus-get-player-property Volume | grep double | awk '{print $3}')"
-			vulumeset="$(perl -e "print "${volume_current}" - "${volumestep}"")"
-			dbus-set-player-property Volume variant:double:"${vulumeset}" >/dev/null
-			;;
-		volume)
-			set -- $@
-			if [[ $# -eq 2 ]]; then
-				dbus-set-player-property Volume variant:double:$2 >/dev/null
-			else
-				dbus-get-player-property Volume | grep double | awk '{print $3}'
-			fi
-			;;
-		status)
-			status=$(dbus-get-player-property PlaybackStatus)
-			<<<"$status" grep string | awk -F\" '{print $2}'
-			;;
-		tracks)
-			tracks=$(dbus-get-tracklist-property Tracks)
-			<<<"$tracks" grep object | awk -F\" '{print $2}'
-			;;
-		add-track)
-			set -- $@
-			play=false
-			[ $# -ge 2 ] && file="$2"
-			[ $# -eq 3 ] && play="$3"
-			absolute_file=$(dirname $(readlink -e "$file"))/$(basename "$file")
-			lastTrack=$($executable $player tracks | tail -n 1)
-			[ -z "$lastTrack" ] && lastTrack="/org/mpris/MediaPlayer2/TrackList/NoTrack"
-			if [[ -f "$absolute_file" ]]; then
-				dbus-tracklist-command AddTrack string:"file://${absolute_file}" \
-					objpath:$lastTrack \
-					boolean:$play > /dev/null
-			fi
-			;;
-		*)
-			echo "'$1' is not a valid command"
-			exit 1
-			;;
-	esac
+function execute-command {
+    case $1 in
+        raise)
+            dbus-root-command Raise       > /dev/null ;;
+        quit)
+            dbus-root-command Quit        > /dev/null ;;
+        play)
+            dbus-player-command Play      > /dev/null ;;
+        pause)
+            dbus-player-command Pause     > /dev/null ;;
+        stop)
+            dbus-player-command Stop      > /dev/null ;;
+        play-pause)
+            dbus-player-command PlayPause > /dev/null ;;
+        next)
+            dbus-player-command Next      > /dev/null ;;
+        prev|previous)
+            dbus-player-command Previous  > /dev/null ;;
+        volumeup)
+            set -- $@
+            if [ -z "${2}" ];then
+                volumestep="0.05"
+            else
+                volumestep="${2}"
+            fi
+            volume_current="$(dbus-get-player-property Volume | grep double | awk '{print $3}')"
+            vulumeset="$(perl -e "print "${volume_current}" + "${volumestep}"")"
+            dbus-set-player-property Volume variant:double:"${vulumeset}" >/dev/null
+            ;;
+        volumedown)
+            set -- $@
+            if [ -z "${2}" ];then
+                volumestep="0.05"
+            else
+                volumestep="${2}"
+            fi
+            volume_current="$(dbus-get-player-property Volume | grep double | awk '{print $3}')"
+            vulumeset="$(perl -e "print "${volume_current}" - "${volumestep}"")"
+            dbus-set-player-property Volume variant:double:"${vulumeset}" >/dev/null
+            ;;
+        volume)
+            set -- $@
+            if [ $# -eq 2 ] ; then
+                dbus-set-player-property Volume variant:double:$2 >/dev/null
+            else
+                dbus-get-player-property Volume | grep double | awk '{print $3}'
+            fi
+            ;;
+        status)
+            status=$(dbus-get-player-property PlaybackStatus)
+            <<<"$status" grep string | awk -F\" '{print $2}'
+            ;;
+        tracks)
+            tracks=$(dbus-get-tracklist-property Tracks)
+            <<<"$tracks" grep object | awk -F\" '{print $2}'
+            ;;
+        add-track)
+            set -- $@
+            play=false
+            [ $# -ge 2 ] && file="$2"
+            [ $# -eq 3 ] && play="$3"
+            absolute_file=$(dirname $(readlink -e "$file"))/$(basename "$file")
+            lastTrack=$($executable $player tracks | tail -n 1)
+            [ -z "$lastTrack" ] && lastTrack="/org/mpris/MediaPlayer2/TrackList/NoTrack"
+            if [ -f "$absolute_file" ] ; then
+                dbus-tracklist-command AddTrack string:"file://${absolute_file}" \
+                    objpath:$lastTrack \
+                    boolean:$play > /dev/null
+            fi
+            ;;
+        *)
+            echo "'$1' is not a valid command"
+            exit 1
+            ;;
+    esac
 }
 
 
 # Parse parameters, or exit with help if none
-if [[ $# -gt 0 ]]; then parse-parameters "$@" ; else
-	echo "Error: Missing parameters" >&2
-	show-help 1
+if [ $# -gt 0 ] ; then parse-parameters "$@" ; else
+    echo "Error: Missing parameters" >&2
+    show-help 1
 fi
 
 
 # Validate the specified player
-if ! is-valid-player "$player"; then
-	echo "PLAYER '$player' is not a valid MPRIS target."
-	echo "List of valid MPRIS targets: "
-	list-valid-player-targets
-	exit 1
+if ! is-valid-player "$player" ; then
+    echo "PLAYER '$player' is not a valid MPRIS target."
+    echo "List of valid MPRIS targets: "
+    list-valid-player-targets
+    exit 1
 fi
 
 execute-command "$command" "$leftovers"
